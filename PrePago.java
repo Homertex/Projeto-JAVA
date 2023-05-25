500e9b46bfb93bd65fdd2820b8f5bcd5519e0f92
lemoslucio <larissalemoslucio@gmail.com>
2023-05-25 09:01:13 -0300

    metodo fazerchamada
    criação do metodo fazer chamada, correção no construtor para acrescimo dos atributos pais

--- PrePago.java
+++ PrePago.java
@@ -5,9 +5,26 @@ public class PrePago{
     private Recarga[] recargas;
 
     public PrePago(float recargas, int numRecargas, int creditos, long cpf, String nome, String numero){
+        super(cpf, nome, numero);
         this.cpf = cpf;
         this.nome = nome;
         this.numero = numero;
         this.recargas = new Recarga[100];
     }
+
+     public void fazerChamada(GregorianCalendar data, int duracao) {
+        Chamada chamada = new Chamada(data, duracao);
+        double custoChamada = 1.45 * duracao;
+
+        if (getNumChamadas() < 100 && creditos >= custoChamada) {
+            realizarChamada(chamada);
+            creditos -= custoChamada;
+            System.out.println("SUCESSO");
+        } else {
+            System.out.println("SALDO INSUFICIENTE");
+        }
+    }
+
+
+
 }
